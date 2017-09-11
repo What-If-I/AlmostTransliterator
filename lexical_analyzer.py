ca456accdcfd481ec6af19d31e15c8faa483dbec
What-If-I <What-If-I@users.noreply.github.com>
2017-09-11 17:25:58 +0300

    Add support of comments and ability to read not only digits and letters

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -1,6 +1,22 @@
+from io import StringIO
+
 from automation import digit_automation, word_automation
 from enums import TToken, literal, TstateEnum, CharTypesEnum
 from transliterator import Transliterator
+
+
+class MyStringIO(StringIO):
+
+    def get_char(self):
+        return self.read(1)
+
+    def shift_position(self, idx: int):
+        cur_position = self.tell()
+        shift = cur_position + idx
+        self.seek(shift, 0)
+
+    def __iter__(self):
+        return iter(lambda: self.get_char(), "")
 
 
 class WrongWordError(Exception):
@@ -11,56 +27,60 @@ class LexicalAnalyzer:
 
     def __init__(self):
         self.current_word = ""
-        self.word_type = TToken.word
-        self.current_automate = None
-        self.current_word_type = None
         self.transliterator = Transliterator()
         self.state = TstateEnum.start
         self.words = []
 
-    def determine_automate(self, symbol):
-        if symbol.type == CharTypesEnum.digit:
-            self.current_automate = digit_automation()
-        elif symbol.type == CharTypesEnum.letter:
-            self.current_automate = word_automation()
+    def _append_word(self, word):
+        self.words.append(word)
 
-    def determine_word_type(self, symbol):
-        if symbol.type == CharTypesEnum.digit:
-            self.current_word_type = TToken.number
-        elif symbol.type == CharTypesEnum.letter:
-            self.current_word_type = TToken.word
+    def _parse_word(self, io_stream: MyStringIO):
+        word = ''
+        word_type = None
+        automate = None
 
-    def reset_automate_and_word(self):
-        self.current_word = ""
-        self.current_automate = None
-
-    def append_word(self):
-        self.words.append(literal(self.current_word, self.current_word_type))
-        self.reset_automate_and_word()
-
-    def analyze(self, string: str):
-        for char in string:
+        for char in io_stream:
             symbol = self.transliterator.get_symbol(char)
 
-            if not self.current_automate:
-                self.determine_automate(symbol)
-                self.determine_word_type(symbol)
+            if symbol.type not in (CharTypesEnum.digit, CharTypesEnum.letter):
+                # undo char read and return
+                io_stream.shift_position(-1)
+                break
 
-            if symbol.type in (CharTypesEnum.space, CharTypesEnum.end_row):
-                if self.current_automate.in_final_state:
-                    self.append_word()
-                else:
-                    raise WrongWordError("Неправильное слово!")
-            elif symbol.type == CharTypesEnum.digit:
-                self.current_automate.next_state(symbol.value)
-                self.current_word += char
+            if not automate:
+                if symbol.type == CharTypesEnum.digit:
+                    automate = digit_automation()
+                    word_type = TToken.number
+                elif symbol.type == CharTypesEnum.letter:
+                    automate = word_automation()
+                    word_type = TToken.word
+
+            if symbol.type == CharTypesEnum.digit:
+                automate.next_state(symbol.value)
+                word += char
             elif symbol.type == CharTypesEnum.letter:
-                self.current_automate.next_state(symbol.value)
-                self.current_word += char
-            else:
-                raise WrongWordError("Неправильное слово!")
+                automate.next_state(symbol.value)
+                word += char
 
-        if self.current_automate.in_final_state:
-            self.append_word()
+        if automate.in_final_state:
+            return literal(word, word_type)
         else:
             raise WrongWordError("Неправильное слово!")
+
+    def analyze(self, string: str):
+        with MyStringIO(string) as string_io:
+            for char in string_io:
+                symbol = self.transliterator.get_symbol(char)
+
+                if symbol.type in (CharTypesEnum.digit, CharTypesEnum.letter):
+                    string_io.shift_position(-1)
+                    word = self._parse_word(string_io)
+                    self._append_word(word)
+                elif symbol.type == CharTypesEnum.comment:
+                    # skip line
+                    string_io.readline()
+                elif symbol.type == CharTypesEnum.space:
+                    # skip space
+                    pass
+                else:
+                    self._append_word(symbol)
